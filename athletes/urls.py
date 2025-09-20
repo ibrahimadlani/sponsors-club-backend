35137c128e980def0423d79de02343e1f0ad14ec
Ibrahim Adlani <77414477+ibrahimadlani@users.noreply.github.com>
2025-09-20 12:43:33 +0100

    Add React messaging tester page

--- athletes/urls.py
+++ athletes/urls.py
@@ -3,12 +3,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import AthleteViewSet, SportListView
+from .views import AthleteViewSet, MyAthletesView, SportListView
 
 router = DefaultRouter()
 router.register(r"athletes", AthleteViewSet, basename="athlete")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("me/athletes/", MyAthletesView.as_view(), name="my-athletes"),
     path("sports/", SportListView.as_view(), name="sports-list"),
 ]
