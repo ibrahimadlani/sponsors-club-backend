f5984628bcfeb4a736e3626c4893a1654d95d784
Ibrahim Adlani <77414477+ibrahimadlani@users.noreply.github.com>
2025-09-21 01:46:27 +0100

    Restore Channels ASGI setup

--- core/asgi.py
+++ core/asgi.py
@@ -9,8 +9,24 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from messaging.middleware import JWTAuthMiddlewareStack
+from messaging.routing import websocket_urlpatterns
+
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": JWTAuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns,
+            )
+        ),
+    }
+)
---
+++ messaging/routing.py
@@ -0,0 +1,9 @@
+"""Routing configuration for messaging websocket connections."""
+
+from __future__ import annotations
+
+from typing import List
+
+from django.urls import URLPattern
+
+websocket_urlpatterns: List[URLPattern] = []
