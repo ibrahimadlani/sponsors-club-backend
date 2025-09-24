89a7d0dcde729f5b872ee6c73fd16a4c52c7c9f9
Ibrahim Adlani <77414477+ibrahimadlani@users.noreply.github.com>
2025-09-24 18:43:04 +0100

    Avoid duplicate is_mandatory column when migrating

--- core/settings.py
+++ core/settings.py
@@ -96,6 +96,17 @@ DATABASES = {
 }
 
 
+if os.environ.get("POSTGRES_DB"):
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ.get("POSTGRES_USER", ""),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
