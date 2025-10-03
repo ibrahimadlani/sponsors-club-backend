224bdf37eb7698dfd2cd89465f22c0dd07961362
Ibrahim Adlani <ibrahim@adlani.com>
2025-10-03 16:36:04 +0100

    Ruff check fixed

--- organisations/signals.py
+++ organisations/signals.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from .models import Collaborator, Organisation
+from .models import Collaborator
 
 
 @receiver(post_delete, sender=Collaborator)
