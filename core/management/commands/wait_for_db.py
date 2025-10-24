34087b8804981eb99040dd64d23b40d07bbd23ef
Ibrahim Adlani <77414477+ibrahimadlani@users.noreply.github.com>
2025-10-24 15:29:21 +0100

    Harden wait_for_db command

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -5,22 +5,33 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management.base import BaseCommand
+from django.db import connections
 from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
+    DEFAULT_SLEEP_SECONDS = 1
+    MAX_SLEEP_SECONDS = 5
+
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
-        db_up = False
-        while db_up is False:
+
+        sleep_seconds = self.DEFAULT_SLEEP_SECONDS
+        while True:
+            connection = connections["default"]
             try:
-                self.check(databases=["default"])
-                db_up = True
+                connection.ensure_connection()
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
-                time.sleep(1)
+                self.stdout.write(
+                    f"Database unavailable, waiting {sleep_seconds} second(s)..."
+                )
+                time.sleep(sleep_seconds)
+                sleep_seconds = min(sleep_seconds * 2, self.MAX_SLEEP_SECONDS)
+            else:
+                connection.close()
+                break
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
